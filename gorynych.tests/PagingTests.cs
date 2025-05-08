99ae569ad73c356040db5058b9551264a3e1a0ac
Дмитрий Навроцкий <dv.navrotskiy@ya.ru>
2025-05-08 15:09:38 +0300

    Тестировать на уровне модульных тестов тут нечего, только нормализацию паджинации, но это же сферический микросервис в вакууме

---
+++ gorynych.tests/PagingTests.cs
@@ -0,0 +1,23 @@
+﻿using gorynych.api.Contracts;
+using Xunit;
+
+namespace gorynych.tests;
+
+public class PagingTests
+{
+    [Theory]
+    [InlineData(0, 0, 10, 1, 10)]
+    [InlineData(-2, -10, 10, 1, 10)]
+    [InlineData(1, 5, 10, 1, 5)]
+    [InlineData(3, 5, 10, 2, 5)]
+    [InlineData(4, 10, 21, 3, 10)]
+    public void TestNormalization(int page, int pageSize, int total, int pageResult, int pageSizeResult)
+    {
+        var paging = new Paging { Page = page, PageSize = pageSize };
+
+        paging.Normalize(total);
+        
+        Assert.Equal(pageResult, paging.Page);
+        Assert.Equal(pageSizeResult, paging.PageSize);
+    }
+}
